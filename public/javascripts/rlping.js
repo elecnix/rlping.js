569df9a1ba9f3c0d4183e48d20a828497db69b7a
Nicolas Marchildon <nicolas@marchildon.net>
2013-11-23 01:12:28 -0500

    Display error received from server

--- public/javascripts/rlping.js
+++ public/javascripts/rlping.js
@@ -2,6 +2,10 @@ angular.module('rlping', ['restangular', 'ui.bootstrap']).
   controller('PingCtrl', function($scope, Restangular) {
     $scope.pings = [];
     Restangular.all('pings').getList({}).then(function(pings) {
+      if (pings.errno) {
+        alert("Oops! " + pings.code + " " + pings.errno + " " + pings.syscall);
+        return;
+      }
       $scope.pings = pings;
     }, function errorCallback() {
       alert("Oops error from server :(");
